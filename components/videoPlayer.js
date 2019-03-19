e5fb60297a8747b9b447684434d32a2c9e9b87ca
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2019-03-18 21:19:23 -0700

    videoPlayer.js

---
+++ components/videoPlayer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native';
+
+export class videoPlayer extends Component {
+  render() {
+    return (
+      <div>
+        <View>
+            <Text>
+              Video Player  
+            </Text>
+        </View>
+      </div>
+    )
+  }
+}
+
+export default videoPlayer
