fc01dff77520656f6b5ac483b16eccd2b03ba298
zachary toth <zacharytoth@zacharys-MacBook-Air.local>
2019-03-19 20:39:37 -0700

    added profile.js

---
+++ components/profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <View>
+            <Text>Welcome Back!</Text>
+        </View>
+      </div>
+    )
+  }
+}
+
+export default Profile
